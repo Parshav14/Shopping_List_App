9e2163e103278b0a8436832b3347466cc799a317
Parshav <Parshav769@gmail.com>
2024-04-17 02:19:17 +0530

    Added Drop-Down Button

--- lib/widgets/new_item.dart
+++ lib/widgets/new_item.dart
@@ -1,4 +1,8 @@
+import 'dart:ffi';
+
 import 'package:flutter/material.dart';
+import 'package:shopping_list/data/categories.dart';
+import 'package:shopping_list/models/category.dart';
 
 class NewItem extends StatefulWidget {
   const NewItem({super.key});
@@ -22,14 +26,47 @@ class _NewItemState extends State<NewItem> {
           child: Column(
             children: [
               TextFormField(
-                  maxLength: 50,
-                  decoration: const InputDecoration(
-                    label: Text('Item Name'),
-                  ),
-                  validator: (value) {
-                    return 'Demo...';
-                  },
+                maxLength: 50,
+                decoration: const InputDecoration(
+                  label: Text('Item Name'),
                 ),
+                validator: (value) {
+                  return 'Demo...';
+                },
+              ),
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: [
+                  Expanded(
+                    child: TextFormField(
+                      decoration: const InputDecoration(
+                        label: Text('Quantity'),
+                      ),
+                      initialValue: '1',
+                    ),
+                  ),
+                  const SizedBox(width: 8.5),
+                  Expanded(
+                    child: DropdownButtonFormField(items: [
+                      for (final category in categories.entries)
+                        DropdownMenuItem(
+                          value: category.value,
+                          child: Row(
+                            children: [
+                              Container(
+                                width: 16,
+                                height: 20,
+                                color: category.value.color,
+                              ),
+                              const SizedBox(width: 6.5),
+                              Text(category.value.title),
+                            ],
+                          ),
+                        )
+                    ], onChanged: (value) {}),
+                  ),
+                ],
+              )
             ],
           ),
         ),
